83cab8e995d7f37ac466ec1842c4c45c76abb594
Fabio <thebloodshadow14@gmail.com>
2023-10-09 10:22:36 +0200

    FIX - js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ domResetBtn.addEventListener("click", resetBoxes);
 
 function getBoxes() {
   const numberIsValid = !domNumber.value || isNaN(domNumber.value) || domNumber.value <= 0 || domNumber.value > 100;
+
   if (numberIsValid) {
     alert("Error... Max number 100, Min number 1.");
     return;
@@ -21,20 +22,17 @@ function getBoxes() {
     if (i % 5 === 0 && i % 3 === 0) {
       domBox.innerHTML = "fizzbuzz";
       domBox.classList.add("bg-[#f0466f]", "box");
-      domGrid.append(domBox);
     } else if (i % 3 === 0) {
       domBox.innerHTML = "fizz";
       domBox.classList.add("bg-[#0cd6a1]", "box");
-      domGrid.append(domBox);
     } else if (i % 5 === 0) {
       domBox.innerHTML = "buzz";
       domBox.classList.add("bg-[#ffd166]", "box");
-      domGrid.append(domBox);
     } else {
       domBox.innerHTML = i;
       domBox.classList.add("bg-[#1389b2]", "box");
-      domGrid.append(domBox);
     }
+    domGrid.append(domBox);
   }
   return;
 }
